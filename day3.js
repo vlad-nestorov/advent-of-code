2efbcf072527c93b93011418e8b47a391dd91e36
Vlad Nestorov <vlad@nestorov.net>
2022-12-05 08:51:20 -0500

    day 3 part 1 done

--- day3.js
+++ day3.js
@@ -1,6 +1,6 @@
 const {sum} = require("./utils");
 
-const part1 = (input) => {
+const findRogueItem = (input) => {
     const comp1 = new Set(input.substr(0, input.length / 2));
     const comp2 = new Set(input.substr(input.length / 2));
     for (const i of comp2) {
@@ -8,6 +8,15 @@ const part1 = (input) => {
             return i;
         }
     }
+    throw `Could not find rogue item in [${input}]`;
 }
 
-module.exports = { part1 };
+const findPriority = (item) => parseInt(item, 36) - 9 + (item === item.toLowerCase() ? 0 : 26);
+
+const part1 = (input) => input.split("\r\n")
+        .map(findRogueItem)
+        .map(findPriority)
+        .reduce(sum)
+
+
+module.exports = { part1, findRogueItem };
--- day3.test.js
+++ day3.test.js
@@ -1,18 +1,33 @@
-const {part1} = require("./day3");
+const {part1, findRogueItem} = require("./day3");
 const {readFileSync} = require('node:fs');
 
 
 describe('Day 3', function () {
 
-    it.each([
+    const exmapleCases = [
         ['vJrwpWtwJgWrhcsFMMfFFhFp', 'p'],
         ['jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL', 'L'],
         ['PmmdzqPrVvPwwTWBwg', 'P'],
         ['wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn', 'v'],
         ['ttgJtRGJQctTZtZT', 't'],
         ['CrZsJsPPZsGzwwsLwLmpwMDw', 's']
-    ])('part1 ', (input, expected) => {
-        expect(part1(input)).toEqual(expected);
+    ]
 
+    const exampleInput = exmapleCases.reduce((prev, [s,..._]) => [...prev, s], [])
+        .join("\r\n")
+
+
+    it.each(exmapleCases)('findRogueItem ', (input, expected) => {
+        expect(findRogueItem(input)).toEqual(expected);
     });
+
+    it('part1 example', () => {
+        expect(part1(exampleInput)).toEqual(157);
+    })
+
+    it('part1 result', () => {
+        const input = readFileSync('day3_sample_input.txt', 'utf8');
+        expect(part1(input)).toEqual(7821);
+    })
+
 });
