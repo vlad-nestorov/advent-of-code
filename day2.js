dfebc1570e74ddb77429bba1c1c22d97306371ed
Vlad Nestorov <vlad@nestorov.net>
2022-12-04 22:49:37 -0500

    day 2 part 2 done

--- day2.js
+++ day2.js
@@ -1,6 +1,9 @@
 const {sum} = require("./utils");
 
 const rpsResult = ([a, x]) => 1 - ((4 + a - x) % 3);
+const rpsChoice = ([a, x]) => (a + 2 + x) % 3;
+
+const part2Score = ([a, x]) => x * 3 + rpsChoice([a, x]) + 1;
 
 const [charA, charX] = ['A', 'X'].map(c => c.charCodeAt(0));
 
@@ -10,4 +13,11 @@ const part1 = (input) =>  input.split("\r\n")
     .map(round => (rpsResult(round) + 1) * 3 + round[1] + 1)
     .reduce(sum)
 
-module.exports = {rpsResult, part1};
+const part2 = (input) =>  input.split("\r\n")
+    .map(value => [0, 2].map(index => value.charCodeAt(index)))
+    .map(([a, x]) => [a - charA, x - charX])
+    .map(part2Score)
+    .reduce(sum)
+
+
+module.exports = {rpsChoice, rpsResult, part2Score, part1, part2};
--- day2.test.js
+++ day2.test.js
@@ -1,4 +1,4 @@
-const {part1, rpsResult} = require("./day2");
+const {part1, part2, part2Score, rpsChoice, rpsResult} = require("./day2");
 const { readFileSync } = require('node:fs');
 
 
@@ -15,13 +15,42 @@ describe('Day 2', function () {
         [0, 2, -1],
         [1, 2, 1]
     ])
-    ('win(%i, %i) returns %i', (a, x, result) => {
+    ('rpsResult(%i, %i) returns %i', (a, x, result) => {
         expect(rpsResult([a, x])).toEqual(result);
     });
+
+    it.each([
+        [0, 0, 2],
+        [1, 0, 0],
+        [2, 0, 1],
+        [0, 1, 0],
+        [1, 1, 1],
+        [2, 1, 2],
+        [0, 2, 1],
+        [1, 2, 2],
+        [2, 2, 0]
+    ])
+    ('rpsChoice(%i, %i) returns %i', (a, x, result) => {
+        expect(rpsChoice([a, x])).toEqual(result);
+    })
 
     it('part 1 returns correct result', () => {
         const input = readFileSync('day2_sample_input.txt', 'utf-8');
         expect(part1(input)).toEqual(14069)
+    })
+
+    it.each([
+        [0,1,4],
+        [1,0,1],
+        [2,2,7]
+    ])
+    ('part2Score([%i, %i]) returns %i', (a, x, expected) => {
+        expect(part2Score([a, x])).toEqual(expected)
+    })
+
+    it('part 2 returns correct result', () => {
+        const input = readFileSync('day2_sample_input.txt', 'utf-8');
+        expect(part2(input)).toEqual(12411)
     })
 
 });
