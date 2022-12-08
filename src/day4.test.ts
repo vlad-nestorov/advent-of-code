41d8a5776e733c7a0f66db82553a9ca5a20cf3a4
Vlad Nestorov <vlad@nestorov.net>
2022-12-07 22:43:08 -0500

    day4 part 1

--- src/day4.test.ts
+++ src/day4.test.ts
@@ -1,7 +1,23 @@
-import {splitPairs} from "./day4";
+import {containsRange, part1, prepInput, Range} from "./day4";
+import { readFileSync } from 'node:fs'
 
 describe('Day 4', function () {
-    it('should ', function () {
-        expect(splitPairs('1-3,2-4')).toEqual([1, 3, 2, 4]);
+    it('prepInput should split ranges correctly ', function () {
+        expect(prepInput('1-3,2-4')).toEqual([[1, 3], [2, 4]]);
     });
+
+    it.each([
+        [true, [[1, 4], [2, 3]]],
+        [true, [[1, 3], [1, 4]]],
+        [true, [[1, 4], [1, 4]]],
+        [false, [[1, 4], [2, 5]]],
+        [false, [[2, 5], [1, 4]]]
+    ])('containsRange is %s for %s', (expected, ranges) => {
+        expect(containsRange(ranges as [Range, Range])).toEqual(expected);
+    });
+
+    it('part 1 answer is correct', () => {
+        const input = readFileSync('src/day4_sample_input.txt', 'utf-8');
+        expect(part1(input)).toEqual(453);
+    })
 });
--- src/day4.ts
+++ src/day4.ts
@@ -1,1 +1,17 @@
-export const splitPairs = (input: string) => input.split(/[-,]/)
+export type Range = [number, number]
+
+export const prepInput = (input: string) =>
+    input.split(",")
+        .map(range => range
+            .split("-")
+            .map(Number)) as [Range, Range];
+
+export const containsRange = (ranges: [Range, Range]) => {
+    const [r1, r2] = ranges;
+    return (r1[0] <= r2[0] && r1[1] >= r2[1])  ||  (r1[0] >= r2[0] && r1[1] <= r2[1]);
+}
+
+export const part1 = (input:string) => input.split("\r\n")
+    .map(prepInput)
+    .filter(containsRange)
+    .length;
