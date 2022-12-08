65c1a3dc95b018918c507e43659ef319054e881c
Vlad Nestorov <vlad@nestorov.net>
2022-12-07 22:59:42 -0500

    day4 part 2

--- src/day4.test.ts
+++ src/day4.test.ts
@@ -1,4 +1,4 @@
-import {containsRange, part1, prepInput, Range} from "./day4";
+import {containsRange, intersectsRange, part1, part2, prepInput, Range} from "./day4";
 import { readFileSync } from 'node:fs'
 
 describe('Day 4', function () {
@@ -16,8 +16,28 @@ describe('Day 4', function () {
         expect(containsRange(ranges as [Range, Range])).toEqual(expected);
     });
 
+    it.each([
+        [true, [[1, 4], [2, 3]]],
+        [true, [[1, 3], [1, 4]]],
+        [true, [[1, 4], [1, 4]]],
+        [true, [[1, 4], [2, 5]]],
+        [true, [[2, 5], [1, 4]]],
+        [true, [[2, 5], [1, 6]]],
+        [false, [[1, 4], [7, 9]]],
+        [false, [[7, 9], [1, 4]]],
+
+    ])('intersectsRange is %s for %p', (expected, ranges) => {
+        expect(intersectsRange(ranges as [Range, Range])).toEqual(expected);
+    });
+
+
     it('part 1 answer is correct', () => {
         const input = readFileSync('src/day4_sample_input.txt', 'utf-8');
         expect(part1(input)).toEqual(453);
+    });
+
+    it('part 2 answer is correct', () => {
+        const input = readFileSync('src/day4_sample_input.txt', 'utf-8');
+        expect(part2(input)).toEqual(919);
     })
 });
--- src/day4.ts
+++ src/day4.ts
@@ -11,7 +11,19 @@ export const containsRange = (ranges: [Range, Range]) => {
     return (r1[0] <= r2[0] && r1[1] >= r2[1])  ||  (r1[0] >= r2[0] && r1[1] <= r2[1]);
 }
 
+export const intersectsRange = (ranges: [Range, Range]) => {
+    const [r1, r2] = ranges;
+    return (r1[0] <= r2[0] && r2[0] <= r1[1])
+        ||  (r1[0] <= r2[1] && r2[1] <= r1[1]) || containsRange(ranges);
+}
+
+
 export const part1 = (input:string) => input.split("\r\n")
     .map(prepInput)
     .filter(containsRange)
+    .length;
+
+export const part2 = (input:string) => input.split("\r\n")
+    .map(prepInput)
+    .filter(intersectsRange)
     .length;
