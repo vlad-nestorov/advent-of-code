d31be274bdf55bb9db12028e4872d78bd1b87515
Vlad Nestorov <vlad@nestorov.net>
2022-12-09 04:44:39 -0500

    day 8 part 2

--- src/day8/index.test.ts
+++ src/day8/index.test.ts
@@ -5,11 +5,11 @@ describe('Day 8', function () {
 
     it('part 1 answer is correct', () => {
         const input = readFileSync('src/day8/sample_input.txt', 'utf-8');
-        expect(part1(input)).toEqual('');
+        expect(part1(input)).toEqual(1693);
     });
 
     it('part 2 answer is correct', () => {
         const input = readFileSync('src/day8/sample_input.txt', 'utf-8');
-        expect(part2(input)).toEqual('');
+        expect(part2(input)).toEqual(422059);
     });
 });
--- src/day8/index.ts
+++ src/day8/index.ts
@@ -1,36 +1,45 @@
 type Tree = {
     height: number,
-    visible: boolean
+    linesOfSight: number[][];
 }
 
 export const parseInput = (input: string): Tree[][] => {
     return input.split("\r\n")
         .map(line => line.split('')
-            .map(Number)
-            .map(height => ({height, visible: false })));
+            .map(Number))
+        .map((row, rowIndex, forest) =>
+            row.map((height, columnIndex) => {
+                let column: number[] =
+                    forest.reduce((acc, cur) => [...acc, cur[columnIndex]], [])
+                return {
+                    height,
+                    linesOfSight: [
+                        row.slice(columnIndex + 1),
+                        row.slice(0, columnIndex).reverse(),
+                        column.slice(rowIndex + 1),
+                        column.slice(0, rowIndex).reverse()
+                    ]
+                }
+            })
+        );
 }
 
 
 export const part1 = (input: string) => {
-    const forest = parseInput(input);
-    forest.forEach((row, rowIndex, array) => {
-        row.forEach((tree, columnIndex) => {
-            let column: Tree[] =
-                array.reduce((acc, cur) => [...acc, cur[columnIndex]], [])
-            tree.visible =[
-                row.slice(columnIndex + 1),
-                row.slice(0, columnIndex),
-                column.slice(rowIndex + 1),
-                column.slice(0, rowIndex)
-            ].some(set => set.every((otherTree: Tree) => otherTree.height < tree.height))
-
-        })
-    });
-
-    return forest.flatMap(value => value)
-        .filter(t => t.visible)
+    return parseInput(input)
+        .flat()
+        .filter(tree => tree.linesOfSight.some(lineOfSight => lineOfSight.every((otherHeight) => otherHeight < tree.height)))
         .length;
 };
 
 export const part2 = (input: string) => {
+    return parseInput(input)
+        .flat()
+        .map(tree => tree.linesOfSight.map((treeLine) => {
+                const tallerTree = treeLine.findIndex((otherHeight) => otherHeight >= tree.height)
+                return tallerTree === -1 ? treeLine.length : tallerTree + 1;
+            })
+            .reduce((previousValue, currentValue) => previousValue * currentValue, 1))
+        .sort((a, b) => a - b)
+        .pop();
 };
