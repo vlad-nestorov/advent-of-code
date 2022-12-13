c8e57a7a4786381a17854fb1f768166d99d22a44
Vlad Nestorov <vlad@nestorov.net>
2022-12-13 12:04:48 -0500

    update template

--- src/_template/index.test.ts
+++ src/_template/index.test.ts
@@ -1,11 +1,22 @@
 import {readFileSync} from 'node:fs'
 import {part1, part2} from "./index";
 
-describe('Day #', function () {
-    const sampleInput = readFileSync('src/day#/sample_input.txt', 'utf-8');
+const DAY = 0;
+
+describe(`Day ${DAY}`, function () {
+    const sampleInput = readFileSync(`src/day${DAY}/sample_input.txt`, 'utf-8');
+    const exampleInput = readFileSync(`src/day${DAY}/example_input.txt`, 'utf-8');
+
+    it('part 1 answer is correct against example', () => {
+        expect(part1(exampleInput)).toEqual('');
+    });
 
     it('part 1 answer is correct', () => {
         expect(part1(sampleInput)).toEqual('');
+    });
+
+    it('part 2 answer is correct against example', () => {
+        expect(part1(exampleInput)).toEqual('');
     });
 
     it('part 2 answer is correct', () => {
