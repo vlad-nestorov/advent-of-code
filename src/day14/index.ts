f235e430ab4878b264fc656999b3f76d488b8d3f
Vlad Nestorov <vlad@nestorov.net>
2022-12-24 11:21:35 -0500

    Day 14 render playing field

--- src/day14/index.ts
+++ src/day14/index.ts
@@ -16,6 +16,8 @@ export const parseInput = (input: string): Point[][] => input.split("\r\n")
 
 export const isOnLine = (xy: number[], p1: Point, p2: Point) => [0, 1].every(i => xy[i] >= Math.min(p1.xy[i], p2.xy[i]) && xy[i] <= Math.max(p1.xy[i], p2.xy[i]))
 
+export const printPlayingField = (field: boolean[][]) => field.map(row => row.map(blocked => blocked ? '#' : '.').join(" ")).join("\n");
+
 export const part1 = (input: string) => {
     const walls = parseInput(input);
 
@@ -27,12 +29,12 @@ export const part1 = (input: string) => {
         down: Math.max(acc.down, value.xy[1])
     }), {left: 500, right: 500, down: 0})
 
-    const playingField: boolean[][] = intializeArray(bounds.down, (y) =>
-        intializeArray(bounds.right - bounds.left, (x) =>
+    const playingField: boolean[][] = intializeArray(bounds.down + 1, (y) =>
+        intializeArray(bounds.right - bounds.left + 1, (x) =>
          walls.some(wall => wall.some((point, index) => index < wall.length - 1 && isOnLine([x + bounds.left, y], point, wall[index + 1])))
     ));
 
-    return playingField.map(row => row.join(" ")).join("\n");
+    return printPlayingField(playingField);
 }
 
 
