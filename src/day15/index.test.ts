c748b35d525751162ebc1fb038fdfb3dc03a23d3
Vlad Nestorov <vlad@nestorov.net>
2022-12-27 09:12:46 -0500

    Day 15 part 1

---
+++ src/day15/index.test.ts
@@ -0,0 +1,25 @@
+import {readFileSync} from 'node:fs'
+import {part1, part2} from "./index";
+
+const DAY = 15;
+
+describe(`Day ${DAY}`, function () {
+    const sampleInput = readFileSync(`src/day${DAY}/sample_input.txt`, 'utf-8');
+    const exampleInput = readFileSync(`src/day${DAY}/example_input.txt`, 'utf-8');
+
+    it('part 1 answer is correct against example', () => {
+        expect(part1(exampleInput, 10)).toEqual(26);
+    });
+
+    it('part 1 answer is correct', () => {
+        expect(part1(sampleInput, 2000000)).toEqual('');
+    });
+
+    it('part 2 answer is correct against example', () => {
+        expect(part2(exampleInput)).toEqual('');
+    });
+
+    it('part 2 answer is correct', () => {
+        expect(part2(sampleInput)).toEqual('');
+    });
+});
---
+++ src/day15/index.ts
@@ -0,0 +1,64 @@
+import {add, applyToArray, group, subtractArray} from "../utils";
+
+type Position = [x:number, y:number];
+
+type XY = {
+    xy: Position
+}
+type SensorPoint = XY & {
+    type: 'sensor'
+    closestBeacon: Position
+    distance: number
+};
+
+type OtherPoint = XY & {
+    type: 'beacon' | 'air' | 'unknown'
+}
+
+type Point =  SensorPoint | OtherPoint;
+
+
+export const distance = (a: Position, b: Position) => subtractArray(a, b).map(Math.abs).reduce(add);
+
+
+export const parseInput = (input: string): SensorPoint[] => input.split("\r\n")
+    .map(line => line.match(/Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/)!)
+    .flatMap(match => match.slice(1).map(Number))
+    .reduce(group(2), [])
+    .reduce(group(2), [])
+    .map(([xy, closestBeacon]) =>
+            ({
+                xy,
+                closestBeacon,
+                distance: distance(xy as Position, closestBeacon as Position),
+                type: 'sensor'
+            }));
+
+const getBounds = (point: SensorPoint) => ({
+    min: point.xy.map(c => c - point.distance),
+    max: point.xy.map(c => c + point.distance),
+});
+
+export const part1 = (input: string, line: number) => {
+    const sensors = parseInput(input);
+
+    const beaconPositions = new Set(sensors.map(s => s.closestBeacon.toString()));
+
+    const bounds = sensors.map(getBounds).reduce((acc, bound) => ({
+        min: applyToArray(acc.min, bound.min, Math.min),
+        max: applyToArray(acc.max, bound.max, Math.max)
+    }))
+
+    let airCount = 0;
+    for (let point: Position = [bounds.min[0], line]; point[0] < bounds.max[0]; point[0]++) {
+
+        if (!beaconPositions.has(point.toString()) && sensors.some(s => distance(point, s.xy) <= s.distance)) {
+            airCount++;
+        }
+    }
+    return airCount;
+}
+
+export const part2 = (input: string) => {
+    return parseInput(input);
+}
